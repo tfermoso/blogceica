402ced57ca59a8a230284e73c566c901d9cbf029
tfermoso <tomas.fermoso@gmail.com>
2024-06-06 16:22:05 +0200

    config.php configuración eloquent

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// config.php
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Events\Dispatcher;
+use Illuminate\Container\Container;
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => 'localhost',
+    'database'  => 'nombre_base_datos',
+    'username'  => 'nombre_usuario',
+    'password'  => 'contraseña',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+]);
+
+// Set the event dispatcher used by Eloquent models... (optional)
+$capsule->setEventDispatcher(new Dispatcher(new Container));
+
+// Make this Capsule instance available globally via static methods... (optional)
+$capsule->setAsGlobal();
+
+// Setup the Eloquent ORM...
+$capsule->bootEloquent();
