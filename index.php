ca4fadfe99b15b8250d03bdb959d1c3a814fb160
tfermoso <tomas.fermoso@gmail.com>
2024-06-06 16:39:27 +0200

    probando clase User

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require 'vendor/autoload.php';
+require 'config.php';
+
+
+$user = new User;
+$user->name = 'John Doe';
+$user->email = 'john@example.com';
+$user->password = password_hash('password', PASSWORD_BCRYPT);
+$user->save();
