785df957db51006dbe05a8d7f6ed89e0ddda5d77
tfermoso <tomas.fermoso@gmail.com>
2024-06-06 17:15:12 +0200

    modelo post

--- index.php
+++ index.php
@@ -3,9 +3,20 @@ require 'vendor/autoload.php';
 require 'config.php';
 
 use App\Models\User;
-
+use App\Models\Post;
+/*
 $user = new User;
 $user->name = 'John Doe';
 $user->email = 'john@example.com';
 $user->password = password_hash('password', PASSWORD_BCRYPT);
 $user->save();
+
+$post=new Post;
+$post->tittle="Post2";
+$post->body="Cuerpo post2";
+$post->image="imagen2.jpg";
+$post->user_id=1;
+$post->save();
+*/
+$posts = Post::with('user')->orderBy('id', 'desc')->take(10)->get();
+var_dump($posts[0]->tittle . " ".$posts[0]->user->name);
